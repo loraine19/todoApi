eaa3d011c8a50fb525d2ae4a3e05171685b65ba4
loraine <lou.hoffmann@gmail.com>
2024-09-18 17:36:14 +0200

    - enfin

--- src/app/api/categorys/route.ts
+++ src/app/api/categorys/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: Request) {
   });
     return NextResponse.json(newcategory, { status: 201 });
   } catch (error) {
-    console.error('Failed to create category:', error);
+    console.error(error);
     return NextResponse.json({ error: 'Failed to create category' }, { status: 500 });
   }
 }
